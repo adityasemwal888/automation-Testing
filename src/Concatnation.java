35cde7d0531535413737e88c66b6de79157370cb
Aditya Semwal <67799398+adityasemwal888@users.noreply.github.com>
2025-06-22 15:31:12 +0530

    operators

---
+++ src/Concatnation.java
@@ -0,0 +1,12 @@
+public class Concatnation {
+    // concatination is nothing but combing int or string
+    // but here a thing combing string and integer can differ the whole thing
+    //EX:
+    public static void main(String[]args){
+        int a=10;
+        int b=34;
+
+        System.out.println(a+b);
+        System.out.println("a+b");
+    }
+}
---
+++ src/uniary_oprators.java
@@ -0,0 +1,9 @@
+public class uniary_oprators {
+    public static void main(String[]args){
+        //uniary oprators
+        // take an example int a = -1000 here the (-) is an uniary oprator
+        int a=100;
+        int b=-200;
+        System.out.println(b);
+    }
+}
