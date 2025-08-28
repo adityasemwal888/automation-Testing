b1c704b23a9f748b3c274ab33cac14c7f50fdc26
Aditya Semwal <67799398+adityasemwal888@users.noreply.github.com>
2025-08-28 21:03:56 +0530

    New

---
+++ Java/Loops/loop_Ex1.java
@@ -0,0 +1,19 @@
+package Loops;
+import java.util.Scanner;
+
+public class loop_Ex1 {
+   // class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("enter you number");
+            int x =scanner.nextInt();
+            if(x%2 == 0){
+                System.out.println("number is even");
+            } else{
+                System.out.println("number is odd");
+            }
+
+
+        }
+    }
+
